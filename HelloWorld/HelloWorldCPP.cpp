ad660fca7840e826549940d2f87650bf9a9121bc
scott <scott.dorsey@spauldingmedical.com>
2021-09-02 10:34:44 -0500

    first changes

---
+++ HelloWorld/HelloWorldCPP.cpp
@@ -0,0 +1,11 @@
+// HelloWorldCPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome to the World Of Dune!\n";
+}
+//
